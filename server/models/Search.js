5f920e9afc47c4d336e36a4ea29d5e0b41ffabdc
santhoshini3 <santhoshinitadakamalla0@gmail.com>
2025-07-25 22:43:50 +0530

    Delete server directory

--- server/models/Search.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const searchSchema = new mongoose.Schema({
-  city: String,
-  temperature: Number,
-  date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Search', searchSchema);
